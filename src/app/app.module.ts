ba5e96dc7e6c3ea65bef9c1e120e5abaf34b2dbc
JPZ <jeanpaulzune@gmail.com>
2017-03-18 20:07:17 -0400

    Log in working, "saves" username and teacher status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { GodService } from './services/god.service';
+
+//ROOTS
+const routes: Routes =  [
+  { path: 'login', component: AppComponent },
+  { path: 'shuffler', component: AppComponent },
+  { path: 'question', component: AppComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +26,8 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [GodService],
+  // providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,18 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userName: string = '';
+  isTeacher: boolean = false;
 
-  constructor(private theLogin: GodService ) { }
-    newStudent = {};
-    
+  // constructor(private theLogin: GodService ) { }
+  constructor() { }
+
+    newUser = {};
+
   ngOnInit() {
   }
 
   addNewUser (user) {
     const newUser = {
       userName: user.userName,
+      teacher: user.isTeacher,
     }
-    this.theLogin.addNewUser(newUser.userName);
-    console.log(this.theLogin);
+
+    console.log(newUser.userName);
+    console.log(newUser.teacher);
+    // this.theLogin.addNewUser(newUser.userName);
+    // console.log(this.theLogin);
   }
 }
